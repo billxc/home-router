30ff9ebe3a046e426ac27313deaaa69b0598cc29
Xiaochen Wu <wxc9312@gmail.com>
2023-09-22 03:03:24 +0800

    init

---
+++ README.md
@@ -0,0 +1,59 @@
+# Xiaochen's Home Router
+
+This document describes my requirements for the home router, and why. And all the configurations and scripts will be in the repo.
+
+## Goals
+1. The router should be able to connect to the Internet via a WAN port, and support global VPN connecting through the router.
+
+1. The router should have a good performance, reasonable storage, and run a OpenWrt Distribution.
+
+    1. I have many requirements for the router. Also, the #1 consumes many resources, so I need a router with good performance.
+      
+    1. I am enough of the 64MB flash and 512MB RAM. Hey, Router manufacturers. Listen, it is 2023, NOT 2003. Can't you add more memory and flash? The cost of memory and flash storage is very very low now. Anyway, I will have a 64GB m-sata SSD, and 4GB RAM.
+
+    1. OpenWrt gives me the power to customize the router. I can install any packages I want.
+
+1. Router will expose the wanted web server to the Internet.
+1. DDNS support
+1. Wireguard VPN server to connect to the home network
+1. Small dependence on OpenWrt Version
+
+    I may use different OpenWrt versions to test and run at initial time, also I may upgrade the version in the future, so I want to make the configurations as independent as possible.
+
+## No Goals
+1. No WiFi support
+    
+    WiFi will be handled by home WiFi APs. So that I can have a better WiFi coverage using the Mesh WiFi. Also, I do not need to upgrade the router when I want to upgrade the WiFi.
+    
+    The router can have many configurations, but the WiFi APs are not. Also, WiFi-7 is coming. I do not want my router to be outdated when WiFi-7 is coming.
+
+1. No complicated services like NAS, media server, etc.
+
+    I will have a dedicated NAS server, and a dedicated media server. I want my router to be a router, not a server. My network experience is the most important thing. I do not want my server be slow down when other services are running. Put all the eggs in one basket is not a good idea.
+
+## Hardware
+
+A mini PC with 4 network ports.
+
+| Item    | Description               | Note                                                                                      |
+|---------|---------------------------|-------------------------------------------------------------------------------------------|
+| CPU     | Intel Celeron N4100       | 4 cores, 4 threads, 6w TDP.<br> it is enough for a 2.5Gbps network and a VPN under 1Gbps. |
+| RAM     | 4GB                       | 1 of 2 memory slot is used, a future upgrade is possible                                  |
+| Storage | 64GB m-sata SSD           |                                                                                           |
+| Network | 4x 2.5Gbps Ethernet ports | 2.5Gbps for future                                                                        |
+| WiFi    | No                        |                                                                                           |
+
+## Software
+
+### OS
+OpenWrt
+
+### Docker
+I will use [Docker on OpenWrt](https://openwrt.org/docs/guide-user/virtualization/docker_host) to run some services. Some services will require complex configurations, and a long compile chain, and they may break OpenWrt's own packages. So I will use Docker to run them.
+
+### Nginx
+Nginx is used for expose websites to the Internet. I will use it to expose my services to the Internet. OpenWrt's own web server is uhttpd, but it is for the router's own web UI. I do not want to make an isolated web server for my services.
+
+I tried to install nginx with opkg, but failed. Also, Nginx is very likely to be conflict with uhttpd, so I will use Docker to run it.
+
+
---
+++ scripts/update_azure_dns.py
@@ -0,0 +1,191 @@
+import argparse
+import datetime
+import json
+import os
+import ssl
+import shutil
+import ipaddress
+
+import urllib.parse
+import urllib.request
+import logging
+
+
+class AzureConfig:
+    def __init__(self, configDict):
+        self.subscriptionId = configDict["subscriptionId"]
+        self.tenantId = configDict["tenantId"]
+        self.appId = configDict["appId"]
+        self.password = configDict["password"]
+        self.resourceGroupName = configDict["resourceGroupName"]
+
+CONFIG: AzureConfig = None
+
+def init(args):
+    init_logging(args)
+
+    global CONFIG
+    if CONFIG is not None:
+        return
+
+    if args.config is None:
+        raise Exception("No config file specified")
+
+    with open(args.config) as f:
+        config = json.load(f)
+        # validate config
+        for k in ["subscriptionId","tenantId","appId","password","resourceGroupName"]:
+            if k not in config:
+                raise Exception(f"config file missing key: {k}")
+        CONFIG = AzureConfig(config)
+
+def init_logging(args):
+    # Set up root logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    # Set up logging to console
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(args.verbose and logging.DEBUG or logging.INFO)
+    console_formatter = logging.Formatter('%(levelname)s: %(message)s')
+    console_handler.setFormatter(console_formatter)
+    logger.addHandler(console_handler)
+
+    if args.log is None:
+        return
+    
+    log_file = args.log
+    # create log file if not exists
+    if not os.path.exists(log_file):
+        with open(log_file, "w") as f:
+            f.write("")
+
+    # move log file if too big, new name: {log_file}-yyyy-mm-dd-hh-mm-ss
+    if os.path.getsize(log_file) > 1024 * 1024: # 1MB
+        new_name = log_file + datetime.datetime.now().strftime("-%Y-%m-%d-%H-%M-%S")
+        shutil.move(log_file, new_name)
+
+    file_handler = logging.FileHandler(args.log)
+    file_handler.setLevel(logging.DEBUG)
+    file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(file_formatter)
+    logger.addHandler(file_handler)
+
+
+def main(args):
+    logging.info("Starting update_azure_dns.py")
+    logging.info(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    update_dns_record(args.domain,args.ip)
+
+
+# separate the domain name and the zone name
+# www.google.com -> www, google.com
+def split_domain(domain):
+    # No special handling for top domain or top level domains like co.uk
+    # TODO: handle special top level domains like co.uk
+    domain_parts = domain.split(".")
+    recordSetName = domain_parts[0]
+    zoneName = ".".join(domain_parts[1:])
+    return recordSetName, zoneName
+    
+
+def update_dns_record(domain,new_ip):
+    token = getBearerToken()
+    [recordSetName, zoneName] = split_domain(domain)
+    logging.info(f"Updating DNS record {recordSetName} in zone {zoneName} to {new_ip}")
+    formatted_ip = ipaddress.ip_address(new_ip) # validate ip address
+    if formatted_ip.version == 6:
+        record_type = "AAAA" 
+    else:
+        record_type = "A"
+
+    dns_service_url = f"https://management.azure.com/subscriptions/{CONFIG.subscriptionId}/resourceGroups/{CONFIG.resourceGroupName}/providers/Microsoft.Network/dnsZones/{zoneName}/{record_type}/{recordSetName}?api-version=2018-05-01"
+    # Set the request body parameters
+    if formatted_ip.version == 6:
+      request_body = {
+          "properties": {
+              "TTL": 3600,
+              "AAAARecords": [
+                  {
+                      "ipv6Address": formatted_ip.exploded
+                  }
+                  ]
+          }
+      }
+    else:
+      request_body = {
+          "properties": {
+              "TTL": 3600,
+              "ARecords": [
+                  {
+                      "ipv4Address": new_ip
+                  }
+              ]
+          }
+      }
+    # Set the request headers, including the authorization header with the bearer token
+
+    # create headers with bearer token
+    request_headers = {
+        "Authorization": "Bearer " + token, # type: ignore
+        "Content-Type": "application/json"
+    }
+
+    # Make the PUT request to update the DNS A record
+
+    # Make a PUT request using urllib.request
+    req = urllib.request.Request(dns_service_url, data=json.dumps(request_body).encode('utf-8'), headers=request_headers, method='PUT')
+    response = urllib.request.urlopen(req)
+
+    # Check the response status to see if the update was successful
+    if response.status in [200, 201]:
+        print("DNS record successfully updated.")
+    else:
+        print("Error updating DNS record. Status code: %d" %
+              response.status_code)
+        print(response.text)
+
+
+def getBearerToken():
+    client_id = CONFIG.appId
+    client_secret = CONFIG.password
+
+    # Set the Azure token endpoint URL
+    token_endpoint_url = "https://login.microsoftonline.com/f3c77fb7-e702-435b-bfb4-37603d1714b1/oauth2/v2.0/token"
+
+    # Set the request body parameters
+    request_body = f"client_id={client_id}&client_secret={client_secret}&grant_type=client_credentials&scope=https://management.azure.com/.default"
+
+    # Make a POST request to the Azure token endpoint to get the bearer token
+    # response = requests.post(token_endpoint_url, data=request_body)
+
+
+    # Disable SSL verification
+    context = ssl.create_default_context()
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_NONE
+
+    data = request_body.encode('utf-8')
+    req = urllib.request.Request(token_endpoint_url, data=data)
+    response = urllib.request.urlopen(req,context=context)
+    # Extract the bearer token from the response
+    if response.status == 200:
+        response_data = response.read().decode('utf-8')
+        json_data = json.loads(response_data)
+        bearer_token = json_data["access_token"]
+        # print("Bearer token: %s" % bearer_token)
+        return bearer_token
+    else:
+        print("Error getting bearer token. Status code: %d" %
+              response.status_code)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("domain", help="The domain to update" )
+    parser.add_argument("ip", help="The ip value of the domain")
+    parser.add_argument("-c","--config", help="The azure_config file to use", default="azure_config.json")
+    parser.add_argument("-v","--verbose", help="Use log if flag is on", action="store_true")
+    parser.add_argument("-l","--log", help="The log file to use", required=False)
+    args = parser.parse_args()
+    init(args)
+    main(args)
