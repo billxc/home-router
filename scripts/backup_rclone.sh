efbfaec493dbc1976abb848c19804d5aacbc0d47
Xiaochen Wu <wxc9312@gmail.com>
2024-01-06 14:21:30 +0800

    Update backup_rclone.sh

--- scripts/backup_rclone.sh
+++ scripts/backup_rclone.sh
@@ -1,3 +1,3 @@
-file_name=/tmp/backup-$(date +%F).tar.gz
+file_name=/tmp/backup-$(date +"%Y-%m-%d_%H_%M_%S").tar.gz
 sysupgrade -b $file_name
 rclone copy $file_name backup:/openwrt/ # replace with your rclone folder
